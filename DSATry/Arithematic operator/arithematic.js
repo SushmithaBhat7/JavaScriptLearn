b0f26d6ef3f9d6c3cef3d95be7bbbf7d270ea641
unknown <sushmitha.bhat7@gmail.com>
2023-06-02 19:56:26 +0530

    Updating Format Input Tag and Media Tag

---
+++ DSATry/Arithematic operator/arithematic.js
@@ -0,0 +1,54 @@
+// let a = 10;
+// let b = 10;
+
+// // addition
+// let add = a + b; // 30
+
+// // subtract
+// let subtract = a - b; // -10
+
+// // divide
+// let divide = a / b; // .5
+
+// // multiply
+// let multiply = a * b; // 200
+
+// // remainder
+// let remainder = a % b; // 10
+
+// // power
+// let power = a ** b; // 1(19)0
+
+// let powerV2 = Math.pow(4, 3);
+
+// console.log("Add: ", add);
+// console.log("Subtract: ", subtract);
+// console.log("divide: ", divide);
+// console.log("multiply: ", multiply);
+// console.log("remainder: ", remainder);
+// console.log("power: ", power);
+// console.log("powerV2: ", powerV2);
+// console.log(
+//   "Final: ",
+//   add,
+//   subtract,
+//   divide,
+//   multiply,
+//   remainder,
+//   power,
+//   powerV2
+// );
+
+
+let a= 10;
+let b = 20;
+let c= "ABCD";
+let d="qwerty"
+
+let add = a+b;
+let mul = c*d;
+let power = a**b;
+
+console.log ("add :",add);
+console.log ("mul :",mul);
+console.log("power :",power); 
