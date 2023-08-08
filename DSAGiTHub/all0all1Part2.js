009c7e27ac945885e13d1c32788e8970ed48648e
unknown <sushmitha.bhat7@gmail.com>
2023-08-08 21:02:32 +0530

    Optimised code

---
+++ DSAGiTHub/all0all1Part2.js
@@ -0,0 +1,28 @@
+let arr = [1,1,1,0,0,0,1,1,1,1,0,0,0,1,1,0,0,1,0,0,1];
+
+function zerosOnes(arr) {
+    let i = 0;
+    let j = arr.length - 1;
+    let hold = 0;
+
+    while (i < j) {
+        if (arr[i] === 0) {
+            i++;
+        } else if (arr[j] === 1) {
+            j--;
+        } else {
+            // Swap arr[i] and arr[j]
+            hold = arr[i];
+            arr[i] = arr[j];
+            arr[j] = hold;
+            i++;
+            j--;
+        }
+    }
+    
+    return arr;
+}
+
+
+
+console.log(zerosOnes(arr));
