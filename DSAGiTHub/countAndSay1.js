cfdc3b338b5dcc69557d2d18e2f2dc9b20c9dc27
unknown <sushmitha.bhat7@gmail.com>
2023-08-26 12:49:18 +0530

    Added new DSA questions

---
+++ DSAGiTHub/countAndSay1.js
@@ -0,0 +1,31 @@
+const n = 6;
+var countAndSay = function (n) {
+  let n1 = n - 1;
+  let sum = "11";
+
+  if (n === 1) {
+    return "1";
+  } else if (n === 2) {
+    return sum;
+  }
+  for (let i = 3; i <= n; i++) {
+    let count = 0;
+    let arr = sum.split("");
+    let temp = "";
+    sum = "";
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] === arr[j + 1]) {
+        count++;
+      } else {
+        count++;
+        temp = "" + count + arr[j];
+        sum = sum + temp;
+        count = 0;
+        temp = "";
+      }
+    }
+  }
+  return sum;
+};
+
+console.log(countAndSay(n));
