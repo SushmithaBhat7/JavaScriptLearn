ea049923475a7f2a74bd1ee70e7218532410a53c
SushmithaBhat7 <sushmitha.bhat7@gmail.com>
2024-01-30 20:56:46 +0530

    Started Node Project

---
+++ 1.Interview/int3.js
@@ -0,0 +1,5 @@
+var x = [1, 2, 3];
+var y = x;
+// x.length = 0;
+x = [];
+console.log(x, y);
---
+++ Node/Basics/hello.js
@@ -0,0 +1,4 @@
+console.log("hello");
+//Added encrypt, decrypt, hashing
+//Cant get window,alert => DOM related to work
+//console.log(window);
