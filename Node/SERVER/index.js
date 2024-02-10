19055d612c817e9af8bdb71a092808a6f6f889b7
SushmithaBhat7 <sushmitha.bhat7@gmail.com>
2024-02-10 08:03:52 +0530

    Node JS code Update

--- Node/SERVER/index.js
+++ Node/SERVER/index.js
@@ -1,17 +1,25 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((reqest, result) => {
   //   console.log(reqest); => Gives an Object
   //   result.end("Welcome to NODE JS Friends");
+  if (reqest.url === "/favicon.ico") return result.end();
+  const myUrl = url.parse(reqest.url, true);
+  console.log(myUrl);
   const log = `${Date.now()} ${reqest.url} New request \n`;
   fs.appendFile("log.txt", log, (error, data) => {
-    switch (reqest.url) {
+    switch (myUrl.pathname) {
       case "/":
         result.end("Welcome to Home Page");
         break;
       case "/About":
         result.end("I'm sushmitha Bhat");
+        break;
+      case "/Search":
+        const search = myUrl.query.search_query;
+        result.end("here is your results " + search);
         break;
       default:
         result.end("404 Page not found");
