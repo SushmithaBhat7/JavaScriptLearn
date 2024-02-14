616ad4fe016da8ea782458d1a2aefac1d841e6a0
SushmithaBhat7 <sushmitha.bhat7@gmail.com>
2024-02-14 06:49:38 +0530

    Node JS code Update

--- Node/PROJECT-01/index.js
+++ Node/PROJECT-01/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = 8001;
+const fs = require("fs");
 const users = require("./MOCK_DATA.json");
+
+//Middleware - plugin
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/users", (request, response) => {
   const html = `
@@ -19,14 +23,32 @@ app.get("/users", (request, response) => {
 app.get("/api/users", (request, response) => {
   return response.json(users);
 });
-
-app.get("/api/users/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const user = users.find((user) => {
-    return user.id === id;
+app.post("/api/users", (request, response) => {
+  const body = request.body;
+  // console.log("body", body);
+  users.push({ ...body, id: users.length + 1 });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return response.json({ status: "Pending", id: users.length + 1 });
   });
-  return response.json(user);
 });
+
+app
+  .route("/api/users/:id")
+  .get((request, response) => {
+    const id = Number(request.params.id);
+    const user = users.find((user) => {
+      return user.id === id;
+    });
+    return response.json(user);
+  })
+  .post((request, response) => {
+    //Edit User with ID
+    return response.json({ status: "pending" });
+  })
+  .delete((request, response) => {
+    //Delete User with ID
+    return response.json({ status: "Pending" });
+  });
 
 app.listen(PORT, () => {
   console.log(`Server Started at ${PORT}`);
