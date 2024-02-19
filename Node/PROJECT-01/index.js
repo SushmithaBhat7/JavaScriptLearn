bfbeb4e5af873f43735b0f46d363413a2121156a
SushmithaBhat7 <sushmitha.bhat7@gmail.com>
2024-02-19 15:55:06 +0530

    Updated Node JS 1st project

--- Node/PROJECT-01/index.js
+++ Node/PROJECT-01/index.js
@@ -2,10 +2,32 @@ const express = require("express");
 const app = express();
 const PORT = 8001;
 const fs = require("fs");
-const users = require("./MOCK_DATA.json");
+let users = require("./MOCK_DATA.json");
 
 //Middleware - plugin
 app.use(express.urlencoded({ extended: false }));
+
+//Custom Middleware 1
+app.use((request, response, next) => {
+  // console.log("Hello from middleware1"); => Holding request not returning or forwarding
+  // return response.json({ msg: "Middleware 1" }); => Ending Request
+  request.creditCardId = 12345; //=>Making changes to the existing request
+  next(); //Forwarding the request to next middleware
+});
+
+//Custom Middleware 2
+app.use((request, response, next) => {
+  console.log("Middleware 2", request.creditCardId);
+  fs.appendFile(
+    "log.txt",
+    `Date : ${Date.now()}, Method : ${request.method}, Path : ${
+      request.path
+    }, IP : ${request.ip} \n`,
+    (err, data) => {
+      next();
+    }
+  );
+});
 
 app.get("/users", (request, response) => {
   const html = `
@@ -21,14 +43,19 @@ app.get("/users", (request, response) => {
 });
 
 app.get("/api/users", (request, response) => {
+  // request.setHeader("X-UseCase", "Practice");
+  response.setHeader("X-MyName", "Sushmitha Bhat");
   return response.json(users);
 });
 app.post("/api/users", (request, response) => {
   const body = request.body;
   // console.log("body", body);
+  if (!body || !body.first_name || !body.last_name) {
+    response.status(400).json({ msg: "All feilds are required" });
+  }
   users.push({ ...body, id: users.length + 1 });
   fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-    return response.json({ status: "Pending", id: users.length });
+    return response.status(201).json({ status: "Pending", id: users.length });
   });
 });
 
@@ -39,6 +66,7 @@ app
     const user = users.find((user) => {
       return user.id === id;
     });
+    if (!user) return response.status(404).json({ msg: "User Not found" });
     return response.json(user);
   })
   .patch((request, response) => {
@@ -65,7 +93,7 @@ app
     const userList = users.filter((user) => {
       return user.id !== id;
     });
-
+    users = userList;
     fs.writeFile("./MOCK_DATA.json", JSON.stringify(userList), (err, data) => {
       return response.json({ status: "Pending" });
     });
