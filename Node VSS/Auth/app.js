a8b800db5136fc5cb33d67774ff754f5ded05c46
SushmithaBhat7 <sushmitha.bhat7@gmail.com>
2024-04-02 07:27:59 +0530

    Authorization

--- Node VSS/Auth/app.js
+++ Node VSS/Auth/app.js
@@ -1,8 +1,10 @@
-const express = require("express");
+import express from "express";
+import signinRouter from "./src/account/SignIn/signin.routes.js";
 const app = express();
-const PORT = 7001;
+const PORT = 8000;
 
 app.use(express.json());
+app.use("/signin", signinRouter);
 app.listen(PORT, () => {
   `The Server is running in localhost:${PORT}`;
 });
